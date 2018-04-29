14806ed079fa435afbff05ae6e00ff5bb693c667
Unknown <boranti1995@gmail.com>
2018-04-29 14:11:28 +0900

    コミット

---
+++ ProgressTest/Form1.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 107)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(328, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "スタート"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 12)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(328, 89)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(349, 147)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ ProgressTest/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Async Sub progTest()
+        Dim prog = New Progress(Of Integer)(AddressOf onProgressChanged)
+
+        Dim task As Task = Task.Run(
+            Sub()
+                doSomething(prog)
+            End Sub)
+
+        Await task
+    End Sub
+
+    Sub doSomething(ByVal iProg As IProgress(Of Integer))
+        For ProgressCount As Integer = 0 To 99
+            Threading.Thread.Sleep(100)
+            iProg.Report(ProgressCount)
+        Next
+    End Sub
+
+    Sub onProgressChanged(ByVal ProgressCount As Integer)
+        ProgressBar1.Value = ProgressCount
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        progTest()
+    End Sub
+End Class
